7dc3fd025873ffeff73268d98f7bacbde0e12d07
Kumar <prashant.y.kumar@capgemini.com>
2017-12-27 18:35:57 +0530

    rest service implemented

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from "./app2.component";
 import{ProductListComponent} from "./product/product-list.component"
 import {FormsModule} from  "@angular/forms";
 import {ProductService} from "./product/products.service";
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
     imports: [BrowserModule, 
-    FormsModule],
+    FormsModule,
+    HttpClientModule],
     declarations: [AppComponent, ProductListComponent],
     bootstrap: [AppComponent],
     providers: [ProductService]
