f624a0fa7930ca57f543811e592f7806337541c6
Kumar <prashant.y.kumar@capgemini.com>
2017-12-27 13:41:43 +0530

    search function implemented

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,11 @@ import {NgModule} from "@angular/core"
 import {BrowserModule} from "@angular/platform-browser";
 import { AppComponent } from "./app2.component";
 import{ProductListComponent} from "./product/product-list.component"
+import {FormsModule} from  "@angular/forms";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, 
+    FormsModule],
     declarations: [AppComponent, ProductListComponent],
     bootstrap: [AppComponent]
 
